44c9a5c9a1c5ff4aba7b1bc0f058851fe94af051
vaferrere <gadoy512@gmail.com>
2016-02-29 14:45:31 +0100

    Menu principal + demande bluetooth + quitter

--- app/src/main/java/apple/iquizz/MainActivity.java
+++ app/src/main/java/apple/iquizz/MainActivity.java
@@ -1,13 +1,49 @@
 package apple.iquizz;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private Button boutton_recherche ;
+
+    private Button boutton_quitter ;
+
+    private EditText pseudo ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boutton_quitter = (Button) findViewById(R.id.boutton_quitter);
+        boutton_recherche = (Button) findViewById(R.id.boutton_recherche);
+        pseudo = (EditText) findViewById(R.id.pseudo);
+
+        boutton_quitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        boutton_recherche.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                if (mBluetoothAdapter == null) {
+                    // Device does not support Bluetooth
+                }
+                if (!mBluetoothAdapter.isEnabled()) {
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                }
+            }
+        });
     }
 }
